d629f0a416142f675759eb15515d156e6cf0942f
jainaldo <jainaldosilva@gmail.com>
2024-02-01 20:52:20 -0300

    criando pagina dinamica

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/visao/CidadeController.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/visao/CidadeController.java
@@ -1,13 +1,24 @@
 package br.edu.utfpr.cp.espjava.crudcidades.visao;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
 
 @Controller
 public class CidadeController {
 
     @GetMapping("/")
-    public String listar() {
-        return "crud.html";
+    public String listar(Model memoria) {
+        var cidades = Set.of(
+                new Cidade("Curitiba", "Paraná"),
+                new Cidade("Saquarema", "Rio de Janeiro"),
+                new Cidade("Rio de Janeiro", "Rio de Janeiro")
+        );
+
+        memoria.addAttribute("listaCidades", cidades);
+
+        return "/crud";
     }
 }
